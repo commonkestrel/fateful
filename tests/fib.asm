f3c953b876d674c9a88ef5adfd2424c9309fbd57
Kestrel <jettb6@gmail.com>
2023-11-12 22:28:24 -0800

    Add test `asm` program

---
+++ tests/fib.asm
@@ -0,0 +1,16 @@
+@org 0x0000
+@define COUNT 7
+
+fib:
+    mv r0, 0
+    mv r1, 1
+    mv r2, COUNT
+.loop:
+    mv r3, r0
+    add r3, r1
+    mv r0, r1
+    mv r1, r3
+    dec r2
+    jnz c, [.loop]
+.halt:
+    jmp [.halt]
