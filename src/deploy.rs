4b3eb9262117e62e20c3c70fb109b96aaca47805
Kestrel <jettb6@gmail.com>
2023-11-12 21:25:09 -0800

    Preemptively add `deploy` subcommand

---
+++ src/deploy.rs
@@ -0,0 +1,16 @@
+//! Deploys assembly programs to the CPU
+//!
+//! Will be completed once the protocol is solidified.
+
+use clap::Args;
+use thiserror::Error;
+
+#[derive(Debug, Args)]
+pub struct DeployArgs {}
+
+#[derive(Debug, Error)]
+pub enum DeployError {}
+
+pub async fn deploy(_args: DeployArgs) -> Result<(), DeployError> {
+    todo!()
+}
