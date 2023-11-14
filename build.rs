8e1214573c48ced2f94081bb14838c4b521a6bae
Kestrel <jettb6@gmail.com>
2023-11-13 22:45:27 -0800

    Simplify `shadow_rs` generation

--- build.rs
+++ build.rs
@@ -1,7 +1,3 @@
-use std::collections::BTreeSet;
-
 fn main() -> shadow_rs::SdResult<()> {
-    let mut exclude = BTreeSet::new();
-    exclude.insert("COMMIT_EMAIL");
-    shadow_rs::new_deny(exclude)
+    shadow_rs::new()
 }
