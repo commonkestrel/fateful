9310869061d53842e5e3a217bd826928b4ce32fa
Kestrel <jettb6@gmail.com>
2023-11-27 21:17:57 -0800

    Update peripheral API

--- examples/c-peripheral/peripheral.c
+++ examples/c-peripheral/peripheral.c
@@ -4,18 +4,22 @@
 
 uint8_t STATE = 0x00;
 
-__declspec(dllexport) void init(uint8_t n) {
+__declspec(dllexport) void init(uint8_t ports) {
     printf("C init\n");
 }
 
-__declspec(dllexport) uint8_t read(uint8_t n) {
+__declspec(dllexport) uint8_t read(uint8_t port) {
     printf("C read\n");
     return STATE;
 }
 
-__declspec(dllexport) void write(uint8_t n, uint8_t data) {
+__declspec(dllexport) void write(uint8_t port, uint8_t data) {
     printf("C write\n");
     STATE = data;
+}
+
+__declspec(dllexport) void reset(void) {
+    STATE = 0;
 }
 
 __declspec(dllexport) const char* name(void) {
--- examples/zig-peripheral/src/main.zig
+++ examples/zig-peripheral/src/main.zig
@@ -2,21 +2,25 @@ const std = @import("std");
 
 var STATE: u8 = 0;
 
-export fn init(n: u8) void {
-    _ = n;
+export fn init(ports: u8) void {
+    _ = ports;
     std.debug.print("Zig init\n", .{});
 }
 
-export fn read(n: u8) u8 {
-    _ = n;
+export fn read(port: u8) u8 {
+    _ = port;
     std.debug.print("Zig read\n", .{});
     return STATE;
 }
 
-export fn write(n: u8, data: u8) void {
-    _ = n;
+export fn write(port: u8, data: u8) void {
+    _ = port;
     std.debug.print("Zig write\n", .{});
     STATE = data;  
+}
+
+export fn reset() void {
+    STATE = 0;
 }
 
 export fn name() [*:0] const u8 {
