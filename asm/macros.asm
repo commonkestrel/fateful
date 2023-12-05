e160496cd7b8b15d57bf52f6fd194216781f4ee6
Kestrel <jettb6@gmail.com>
2023-12-04 23:42:08 -0800

    Fix macro syntax

--- asm/macros.asm
+++ asm/macros.asm
@@ -1,16 +1,16 @@
 /// Adds two 16-bit integers
 @macro add16 {
-    ($h0:reg, $l0:reg, $h1:reg|imm, $l1:reg|imm) {
-        add $l0, $l1
-        adc $h0, $h1
+    (%h0:reg, %l0:reg, %h1:reg|imm, %l1:reg|imm) {
+        add %l0, %l1
+        adc %h0, %h1
     }
 }
 
 /// Subtracts two 16-bit integers
 @macro sub16 {
-    ($h0:reg, $l0:reg, $h1:reg|imm, $l1:reg|imm) {
-        sub $l0, $l1
-        sbc $h0, $h1
+    (%h0:reg, %l0:reg, %h1:reg|imm, %l1:reg|imm) {
+        sub %l0, %l1
+        sbc %h0, %h1
     }
 }
 
@@ -18,8 +18,8 @@
     () {
         jnz 0
     }
-    ($addr:addr) {
-        lda $addr
+    (%addr:addr) {
+        lda %addr
         jmp
     }
 }
