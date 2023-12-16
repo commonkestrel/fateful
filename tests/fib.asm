233e66fae9c314f743caf72307d1bcfe11e53dc7
commonkestrel <jettb6@gmail.com>
2023-12-15 22:27:57 -0800

    Test `call`

--- tests/fib.asm
+++ tests/fib.asm
@@ -1,18 +1,26 @@
+; calculates the 8th fibonacci number and leaves the result in d
+
 @org 0x0000
-@define COUNT 3
+@define COUNT 7
 
 @include <macros>
 
+_start:
+    mv A, COUNT
+    call [fib]
+    halt
+
 fib:
+    mv C, A
     mv A, 0
     mv B, 1
     mv C, COUNT
 .loop:
     mv D, A
-    add C, B
+    add D, B
     mv A, B
     mv B, D
     dec C
     lda [.loop]
     jnz C
-    halt
+    ret
